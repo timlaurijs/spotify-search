552adcf2d58f583afaa37c7669189fc8a3d934f7
timlaurijs <tim.laurijs@touchtribe.nl>
2025-04-24 23:05:01 +0200

    feat [spotify-search]: fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,16 @@ import { FormSearch } from "components/form-search";
 import { cachedSearchSpotify } from "service/spotify";
 
 interface Props {
-  searchParams: {
+  searchParams: Promise<{
     q?: string;
-  };
+  }>;
 }
 export default async function Home({ searchParams }: Props) {
   const resolvedSearchParams = await searchParams;
-
   const results = resolvedSearchParams?.q
     ? await cachedSearchSpotify(resolvedSearchParams.q)
     : null;
   console.log("results", results);
-
   return (
     <div>
       <h1>HHR</h1>
